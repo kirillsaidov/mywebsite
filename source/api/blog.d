f29d35865c4cfbb7838c12d2b14e761402efb189
Kirill Saidov <kirillsaidovd@yahoo.com>
2025-09-17 20:40:41 +0600

    trying out

--- source/api/blog.d
+++ source/api/blog.d
@@ -1,2 +1,28 @@
 module api.blog;
 
+import std.datetime : DateTime;
+import vibe.web.rest;
+
+struct BlogMetadata
+{
+    uint id;
+    string name;
+    string desc;
+    DateTime created_at;
+    DateTime modified_at;
+}
+
+struct Blog 
+{
+    BlogMetadata metadata;
+    string content;
+}
+
+interface BlogAPI
+{
+    @get @path("/list")
+    BlogMetadata[] list();
+
+    @get @path("/get/:id")
+    Blog get(in uint id);    
+}
