95eb6051a2ebbfa38c0c3aea877b1d1007e12924
Kirill Saidov <kirillsaidovd@yahoo.com>
2025-10-05 13:21:26 +0500

    add: api key config, @safe

--- source/config.d
+++ source/config.d
@@ -2,19 +2,34 @@ module config;
 
 import std.file : readText;
 import std.json : JSONValue, parseJSON;
+import std.process : environment;
 
 /// Website-wide config
 private JSONValue config;
+
+/// API key for website operations that require authentication
+private string apiKey;
 
 static this() 
 {
     // load config
     auto configText = readText("config/config.json");
     config = parseJSON(configText);
+
+    // get api key
+    apiKey = environment.get("API_KEY", null);
 }
+
+@safe:
 
 JSONValue getConfig()
 {
     return config;
 }
+
+string getAPIKey()
+{
+    return apiKey;
+}
+
 
