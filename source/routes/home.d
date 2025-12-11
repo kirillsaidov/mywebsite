36113604342a0355978732a5c93c7a49921f5331
Kirill Saidov <kirillsaidovd@yahoo.com>
2025-12-11 18:42:21 +0500

    update: does to unix save file

--- source/routes/home.d
+++ source/routes/home.d
@@ -1,32 +1,11 @@
 module routes.home;
 
-import std.array : array;
-import std.algorithm.iteration : map;
-
 import vibe.vibe;
-import config;
 
 void getHomePage(HTTPServerRequest req, HTTPServerResponse res)
 {
-    struct AboutInfo {
-        string name;
-        string[] bio;
-        string email_user;
-        string email_domain;
-        string linkedin;
-        string github_ks, github_rk;
-    }
-    
-    auto data = getConfig()["about"];
-    immutable about = AboutInfo(
-        name: data["name"].str,
-        bio: data["bio"].array.map!(item => item.str).array.dup,
-        email_user: data["social"]["email-user"].str,
-        email_domain: data["social"]["email-domain"].str,
-        linkedin: data["social"]["linkedin"].str,
-        github_ks: data["social"]["github_ks"].str,
-        github_rk: data["social"]["github_rk"].str,
-    );
-
-    res.render!("index.dt", about);
+    res.render!("index.dt");
 }
+
+
+
